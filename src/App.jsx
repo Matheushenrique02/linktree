234d8c07da0e85549ecacccfa40bf32d77bf8f1b
Matue777x666 <matheusemail222@gmail.com>
2025-11-04 09:51:43 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen w-screen bg-orange-300">
+    <div className="flex flex-col justify-center items-center min-h-screen w-screen bg-orange-300 overflow-x-hidden">
       
       {/* Conteúdo principal */}
-      <div className="flex flex-col justify-center items-center max-w-[900px] w-[90%] p-5 mt-3 rounded-xl bg-cover bg-center">
+      <div className="flex flex-col justify-center items-center max-w-[700px] w-[90%] p-5 mt-3 rounded-xl bg-cover bg-center">
 
         {/* Foto com animação */}
         <div className="flex justify-center items-center w-[150px] h-[150px] [perspective:1000px]">
